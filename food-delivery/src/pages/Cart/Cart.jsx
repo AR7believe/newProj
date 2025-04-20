7933cd244c7735d925e2ec7da39c1b1940720bbb
K Aman Rao <k.amanrao69@gmail.com>
2025-04-20 16:27:03 +0530

    index removed

--- food-delivery/src/pages/Cart/Cart.jsx
+++ food-delivery/src/pages/Cart/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
+        {food_list.map((item) => {
           if (cartItems[item._id] > 0) {
             return (
               <>
